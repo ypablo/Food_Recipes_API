d170bd59967ed5c43f9a83d30db5af62ae25a55d
ypablo <yfedora@gmail.com>
2020-04-01 01:23:25 +0100

    Fix bugs

--- src/components/Default/Default.js
+++ src/components/Default/Default.js
@@ -7,10 +7,12 @@ export default class Default extends Component {
         return (
             <div className="container-fluid">
                 <div title="404" className="row align-items-center default-hero">
-                    <h2 className="text-light text-uppercase">you are in the wrong place</h2>
-                    <Link to='/' className="text-uppercase bt btn-secondary btn-lg mt-3 nounderline">
-                        return home
-                </Link>
+                    <div className="col-lg-12 col-lx-12 col-12 mx-auto col-md-12 text-center">
+                        <h2 className="text-light text-slanted text-uppercase">you are in the wrong place</h2>
+                        <Link to='/' className="text-uppercase bt btn-secondary btn-lg mt-3 nounderline">
+                            return home
+                        </Link>
+                    </div>
                 </div>
             </div>
         )
